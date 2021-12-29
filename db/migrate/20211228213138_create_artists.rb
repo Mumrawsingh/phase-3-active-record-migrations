da9d0913ea8c512926fdebd159724ec95f385d92
Matthew Umrawsingh <matthew.umrawsingh@gmail.com>
2021-12-29 10:17:38 -0500

    commit tree 1fff15be341b5dda2f28c885470e97857c767663

--- db/migrate/20211228213138_create_artists.rb
+++ db/migrate/20211228213138_create_artists.rb
@@ -1,9 +1,11 @@
-class Artist
-    
+class CreateArtists < ActiveRecord::Migration[6.1]
+  def change
+
     Artist.new(name: 'Justice', 
     genre: 'Electronic', 
     age: Time.now.strftime("%Y").to_i - 2003, 
     hometown: "Paris"
     end
 
+  end
 end
